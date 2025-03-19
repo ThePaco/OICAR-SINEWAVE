046f8eb7c5dad23e75e42ad2c497fa8cc4501069
ThePaco <px_vd@outlook.com>
2025-03-19 19:16:04 +0100

    init

---
+++ src/main/java/hr/spring/web/sinewave/sinewaveapp/SinewaveAppApplication.java
@@ -0,0 +1,13 @@
+package hr.spring.web.sinewave.sinewaveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinewaveAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinewaveAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/spring/web/sinewave/sinewaveapp/SinewaveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.spring.web.sinewave.sinewaveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinewaveAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

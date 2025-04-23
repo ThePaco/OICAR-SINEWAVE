08f1ef6ac0d0dc1164be295460a523853c486fbe
Bruno Ravenščak <bravens@algebra.hr>
2025-04-23 12:51:02 +0200

    Added jwt token authentication

---
+++ src/main/java/hr/spring/web/sinewave/dto/AuthResponseDto.java
@@ -0,0 +1,15 @@
+package hr.spring.web.sinewave.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDto {
+    private String token;
+    private UserDto user;
+}

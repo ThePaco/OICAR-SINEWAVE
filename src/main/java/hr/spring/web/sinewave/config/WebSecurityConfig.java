61a4b23a53d14eb31f41b6f3e7db4812bc8d7d03
tbogdan1 <tbogdan@racunarstvo.hr>
2025-06-11 09:30:00 +0200

    DOdan front url u CORS

--- src/main/java/hr/spring/web/sinewave/config/WebSecurityConfig.java
+++ src/main/java/hr/spring/web/sinewave/config/WebSecurityConfig.java
@@ -69,7 +69,8 @@ public class WebSecurityConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
                 "http://10.0.2.2:3000",
-                "http://192.168.1.237:3000"
+                "http://192.168.1.237:3000",
+                "https://sinewave-frontend.onrender.com"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
